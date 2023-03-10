73d0e8c264be431fe129d9fae39372166718f2d9
Hariom Yadav <hariomask0111@gmail.com>
2023-03-10 22:18:10 +0530

    Add GET UPDATE DELETE DATA IN DATABASE

--- test2/expresswithmongoose/src/ui/index.js
+++ test2/expresswithmongoose/src/ui/index.js
@@ -26,6 +26,75 @@ app.post('/student', async (req, res)=>{
 
 app.get('/', (req, res)=>{
     console.log("Home Page");
+    res.send('This Is Home Page...')
+
+});
+app.get('/studentsdata', async (req, res)=>{
+    console.log("Data Page..");
+    try{
+        const data = await Collection.find();
+        res.send(data);
+    }
+    catch(err){
+        console.log(err)
+    }
+
+});
+
+
+app.get('/studentdatabyid/:id', async (req, res)=>{
+    console.log("Get Data By ID Page..");
+    try{
+        console.log(req.params)
+        const id = req.params.id;
+        const data = await Collection.findById(id);
+        res.send(data);
+    }
+    catch(err){
+        console.log(err)
+    }
+
+});
+app.get('/studentdatabyname/:name', async (req, res)=>{
+    console.log("Get Data By ID Page..");
+    try{
+        console.log(req.params)
+        const name = req.params;
+        const data = await Collection.find(name);
+        res.send(data);
+    }
+    catch(err){
+        console.log(err)
+    }
+
+});
+app.get('/studentdeletdata/:id', async (req, res)=>{
+    console.log("Delete Data By ID Page..");
+    try{
+        console.log(req.params)
+        const id = req.params.id;
+        const data = await Collection.findByIdAndDelete(id);
+        res.send(data);
+    }
+    catch(err){
+        console.log(err)
+    }
+
+});
+
+// update data in database
+app.patch('/studentupdatedata/:id', async (req, res)=>{
+    console.log("Update Data By ID Page...");
+    try{
+        console.log(req.params)
+        const id = req.params.id;
+        const data = await Collection.findByIdAndUpdate(id, req.body, {new: true});   // req.body - postmon,  new: true - updated data get
+        res.send(data);
+    }
+    catch(err){
+        console.log(err)
+    }
+
 });
 
 app.listen(port, (err)=>{
